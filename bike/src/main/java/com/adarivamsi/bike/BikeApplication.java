b798acc25fd41ddefd7a5be1f9ab0767ed569f6d
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-05-01 02:13:32 -0400

    files.

---
+++ bike/src/main/java/com/adarivamsi/bike/BikeApplication.java
@@ -0,0 +1,19 @@
+package com.adarivamsi.bike;
+
+/**
+ * All Copyrights reserved @ Vamsi Charan Adari
+ * Any queries mail me at adarivamsi@gmail.com
+ * Created by adari on 4/30/2020
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeApplication.class, args);
+	}
+
+}
